e615419d335c70b0d3efc18cf637488980a984b9
zachary-jones <zachary-jones@bisk.com>
2016-10-12 18:38:30 -0400

    updates to mobile nav

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,93 +1,99 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var timeout = 2500;
-    $("html").animate({ opacity: 1}, timeout);
+    $("html").animate({
+        opacity: 1
+    }, timeout);
     $('nav').slideDown(timeout);
     openNav(timeout);
-    $(window).resize(function(){
+    $(window).resize(function () {
         openNav(timeout);
     });
-    $(".show").click(function(){
-        $(".menu-items").slideToggle();
+    $(".show").click(function () {
+        $(".menu-items").toggle();
         if ($('html,body').css("overflow") == "hidden") {
-            $('html,body').css("overflow","")
+            $('html,body').css("overflow", "")
         } else {
-            $('html,body').css("overflow","hidden")
+            $('html,body').css("overflow", "hidden");
         }
     });
-    $('.menu-items li').click(function(){
+    $('.menu-items li').click(function () {
         if (window.innerWidth <= 816) {
-            $(".menu-items").slideUp();
+            $(".menu-items").hide();
+            if ($('html,body').css("overflow") == "hidden") {
+                $('html,body').css("overflow", "")
+            } else {
+                $('html,body').css("overflow", "hidden");
+            }
         }
     })
-    setInterval(bouncy,10)
+    setInterval(bouncy, 100)
 });
 
-function openNav(timeout){
+function openNav(timeout) {
     if (window.innerWidth >= 816) {
-        $('html,body').css("overflow","");
+        $('html,body').css("overflow", "");
         $('.menu-items').show();
-        $('.menu-items li').fadeIn(timeout * 1.5).css("display","inline-block");
-        setTimeout(function(){
-            $('footer, footer *').fadeIn(timeout * 1.5).css("display","inline-block");
-        },2000)
+        $('.menu-items li').fadeIn(timeout * 1.5).css("display", "inline-block");
+        setTimeout(function () {
+            $('footer, footer *').fadeIn(timeout * 1.5).css("display", "inline-block");
+        }, 2000)
     }
 }
 
-function bouncy(){
-    if($(window).scrollTop() + $(window).height() >= getDocHeight() - 100) {
+function bouncy() {
+    if ($(window).scrollTop() + $(window).height() >= getDocHeight() - 100) {
         $("#scrollDown").fadeOut();
-    }
-    else {
+    } else {
         $("#scrollDown").fadeIn();
     }
 }
 
 function getDocHeight() {
-   var D = document;
-   return Math.max(
-       D.body.scrollHeight, D.documentElement.scrollHeight,
-       D.body.offsetHeight, D.documentElement.offsetHeight,
-       D.body.clientHeight, D.documentElement.clientHeight
-   );
+    var D = document;
+    return Math.max(
+        D.body.scrollHeight, D.documentElement.scrollHeight,
+        D.body.offsetHeight, D.documentElement.offsetHeight,
+        D.body.clientHeight, D.documentElement.clientHeight
+    );
 }
-    var app = angular.module('ijj', ['ngRoute', 'ngAnimate']);
+var app = angular.module('ijj', ['ngRoute', 'ngAnimate']);
 
-        app.config(function($routeProvider, $locationProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl : '/view/home.html',
-                controller  : 'mainController'
-            })
-            .when('/about', {
-                templateUrl : '/view/about.html',
-                controller  : 'mainController'
-            })
-            .when('/portfolio', {
-                templateUrl : '/view/portfolio.html',
-                controller  : 'mainController'
-            })
-            .when('/experience', {
-                templateUrl : '/view/experience.html',
-                controller  : 'mainController'
-            })
-            .when('/appraise', {
-                templateUrl : '/view/appraise.html',
-                controller  : 'mainController'
-            })
-            .when('/inquire', {
-                templateUrl : '/view/inquire.html',
-                controller  : 'mainController'
-            })
-            .when('/blog', {
-                templateUrl : '/view/blog.html',
-                controller  : 'mainController'
-            })                             
-            .otherwise('/');
-            $locationProvider.html5Mode(true);
-    });
+app.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: '/view/home.html',
+            controller: 'mainController'
+        })
+        .when('/about', {
+            templateUrl: '/view/about.html',
+            controller: 'mainController'
+        })
+        .when('/portfolio', {
+            templateUrl: '/view/portfolio.html',
+            controller: 'mainController'
+        })
+        .when('/experience', {
+            templateUrl: '/view/experience.html',
+            controller: 'mainController'
+        })
+        .when('/appraise', {
+            templateUrl: '/view/appraise.html',
+            controller: 'mainController'
+        })
+        .when('/inquire', {
+            templateUrl: '/view/inquire.html',
+            controller: 'mainController'
+        })
+        .when('/blog', {
+            templateUrl: '/view/blog.html',
+            controller: 'mainController'
+        })
+        .otherwise('/');
+    $locationProvider.html5Mode(true);
+});
 
 
-    // create the controller and inject Angular's $scope
-    app.controller('mainController', function($scope) {
+// create the controller and inject Angular's $scope
+app.controller('mainController', function ($scope) {
 
-    });
+});
